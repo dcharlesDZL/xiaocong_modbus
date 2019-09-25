5973968f51d383d4b6013c1b6e676bcc2cbdfecf
dcharlesDZL <liketwins.ok@163.com>
2019-09-25 10:37:13 +0800

    add

--- edp8266_demo.ino
+++ edp8266_demo.ino
@@ -14,7 +14,7 @@ void loop()
     //向设备请求参数信息
     int message[8] = {0x01,0x03,0x00,0x00,0x00,0x0f,0x05,0xce}; 
     for(int i=0; i<8; i++)     RS485.write(message[i]);
-     delay(1000);
+    delay(1000);
 //    Serial.read();
 //    Serial.write(message);
 //    rs485.read();
